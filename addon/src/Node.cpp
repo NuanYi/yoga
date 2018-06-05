348e1cf287c2f70872f2eee98e3e3a6733561bfe
ruiwen.trw <ruiwen.trw@alibaba-inc.com>
2018-06-05 20:36:14 +0800

    add some function

--- addon/src/Node.cpp
+++ addon/src/Node.cpp
@@ -19,6 +19,11 @@ namespace yoga {
         tpl->SetClassName(Nan::New("Node").ToLocalChecked());
         tpl->InstanceTemplate()->SetInternalFieldCount(1);
         
+        Nan::SetPrototypeMethod(tpl, "setPositionType", setPositionType);
+        Nan::SetPrototypeMethod(tpl, "setPosition", setPosition);
+        Nan::SetPrototypeMethod(tpl, "setPositionPercent", setPositionPercent);
+        
+        
         Nan::SetPrototypeMethod(tpl, "setWidth", setWidth);
         Nan::SetPrototypeMethod(tpl, "setHeight", setHeight);
         Nan::SetPrototypeMethod(tpl, "setJustifyContent", setJustifyContent);
@@ -137,6 +142,159 @@ namespace yoga {
 
         YGNodeStyleSetAlignContent(node->node(), static_cast<YGAlign>(alignContent));
     }
+    
+    NAN_METHOD(Node::setAlignItems) {
+        Nan::HandleScope();
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int alignItems;
+        NanCheckArguments(info).argumentsCount(1)
+        .argument(0).stringEnum<int>({
+            {"auto", YGAlignAuto},
+            {"flex-start", YGAlignFlexStart},
+            {"center", YGAlignCenter},
+            {"flex-end", YGAlignFlexEnd},
+            {"stretch", YGAlignStretch},
+            {"baseline", YGAlignBaseline},
+            {"space-between", YGAlignSpaceBetween},
+            {"space-around", YGAlignSpaceAround}
+        })
+        .bind(alignItems)
+        .check();
+        
+        YGNodeStyleSetAlignItems(node->node(), static_cast<YGAlign>(alignItems));
+    }
+    
+    NAN_METHOD(Node::setAlignSelf) {
+        Nan::HandleScope();
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int alignSelf;
+        NanCheckArguments(info).argumentsCount(1)
+        .argument(0).stringEnum<int>({
+            {"auto", YGAlignAuto},
+            {"flex-start", YGAlignFlexStart},
+            {"center", YGAlignCenter},
+            {"flex-end", YGAlignFlexEnd},
+            {"stretch", YGAlignStretch},
+            {"baseline", YGAlignBaseline},
+            {"space-between", YGAlignSpaceBetween},
+            {"space-around", YGAlignSpaceAround}
+        })
+        .bind(alignSelf)
+        .check();
+        
+        YGNodeStyleSetAlignSelf(node->node(), static_cast<YGAlign>(alignSelf));
+    }
+    
+    NAN_METHOD(Node::setFlexDirection) {
+        Nan::HandleScope();
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int flexDirection;
+        NanCheckArguments(info).argumentsCount(0)
+        .argument(1)
+        .stringEnum<int>({
+            {"column", YGFlexDirectionColumn},
+            {"column-reverse", YGFlexDirectionColumnReverse},
+            {"row", YGFlexDirectionRow},
+            {"row-reverse", YGFlexDirectionRowReverse}
+        }).bind(flexDirection)
+        .check();
+        
+        YGNodeStyleSetFlexDirection(node->node(), static_cast<YGFlexDirection>(flexDirection));
+    }
+    
+    NAN_METHOD(Node::setFlexWrap) {
+        Nan::HandleScope();
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int wrap;
+        NanCheckArguments(info).argumentsCount(0)
+        .argument(1)
+        .stringEnum<int>({
+            {"none", YGWrapNoWrap},
+            {"wrap", YGWrapWrap},
+            {"wrap-reverse", YGWrapWrapReverse}
+        }).bind(wrap)
+        .check();
+        
+        YGNodeStyleSetFlexWrap(node->node(), static_cast<YGWrap>(wrap));
+    }
+    
+    NAN_METHOD(Node::setJustifyContent) {
+        Nan::HandleScope();
+        
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int justfyContent;
+        NanCheckArguments(info).argumentsCount(1)
+        .argument(0).stringEnum<int>({
+            { "center", YGJustifyCenter },
+            { "flex-start", YGJustifyFlexStart },
+            { "flex-end", YGJustifyFlexEnd },
+            { "space-between", YGJustifySpaceBetween },
+            { "space-around", YGJustifySpaceAround }
+        }).bind(justfyContent)
+        .check();
+        
+        YGNodeStyleSetJustifyContent(node->node(), static_cast<YGJustify>(justfyContent));
+    }
+    
+    NAN_METHOD(Node::setMargin) {
+        Nan::HandleScope();
+        
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int edge;
+        double margin;
+        NanCheckArguments(info).argumentsCount(2)
+        .argument(0)
+        .stringEnum<int>({
+            {"left", YGEdgeLeft},
+            {"right", YGEdgeRight},
+            {"top", YGEdgeTop},
+            {"bottom", YGEdgeBottom},
+            {"start", YGEdgeStart},
+            {"end", YGEdgeEnd},
+            {"horizontal", YGEdgeHorizontal},
+            {"vertical", YGEdgeVertical},
+            {"all", YGEdgeAll}
+        }).bind(edge)
+        .argument(1)
+        .bind(margin)
+        .check();
+        
+        YGNodeStyleSetMargin(node->node(), static_cast<YGEdge>(edge), margin);
+    }
+    
+    NAN_METHOD(Node::setMarginPercent) {
+        Nan::HandleScope();
+        
+        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
+        
+        int edge;
+        double margin;
+        NanCheckArguments(info).argumentsCount(2)
+        .argument(0)
+        .stringEnum<int>({
+            {"left", YGEdgeLeft},
+            {"right", YGEdgeRight},
+            {"top", YGEdgeTop},
+            {"bottom", YGEdgeBottom},
+            {"start", YGEdgeStart},
+            {"end", YGEdgeEnd},
+            {"horizontal", YGEdgeHorizontal},
+            {"vertical", YGEdgeVertical},
+            {"all", YGEdgeAll}
+        }).bind(edge)
+        .argument(1)
+        .bind(margin)
+        .check();
+        
+        YGNodeStyleSetMarginPercent(node->node(), static_cast<YGEdge>(edge), static_cast<float>(margin));
+    }
+    
 
     NAN_METHOD(Node::setHeight) {
         Nan::HandleScope();
@@ -174,24 +332,5 @@ namespace yoga {
             info.GetReturnValue().Set(value.value);
         }
         
-    }
-    
-    NAN_METHOD(Node::setJustifyContent) {
-        Nan::HandleScope();
-        
-        Node* node = Nan::ObjectWrap::Unwrap<Node>(info.Holder());
-        
-        NanCheckArguments(info).argumentsCount(1)
-        .argument(0).stringEnum<int>({
-            { "center", YGJustifyCenter },
-            { "flex-start", YGJustifyFlexStart },
-            { "flex-end", YGJustifyFlexEnd },
-            { "space-between", YGJustifySpaceBetween },
-            { "space-around", YGJustifySpaceAround }
-        })
-        .bind([node](int value) -> void{
-            YGNodeStyleSetJustifyContent(node->node(), static_cast<YGJustify>(value));
-        })
-        .check();
     }
 }
